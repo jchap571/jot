88241b7a8dbce76e5445e32963c1720218de1c87
James Chapman <jchapman605@gmail.com>
2024-08-30 11:33:15 -0600

    list template added to model

--- app/models/Note.js
+++ app/models/Note.js
@@ -10,6 +10,30 @@ export class Note {
   }
 
 
+  get listHTMLTemplate() {
+    return /*html*/`
+        <div class="d-flex justify-content-between">
+                <h3>Note Title</h3>
+                <p>8/30/24</p>
+          </div>
+          <div class="d-flex text-start">
+            <p>
+                  1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, asperiores!
+                  2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque odio quas labore ipsum
+                  cupiditate
+                  explicabo?
+                  3. Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
+          </div>
+        </div>
+          <hr>
+`
+
+
+
+  }
+
+
 
 
 }

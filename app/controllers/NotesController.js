3c4bdb77ccb278fd584b76adc649fada91e7e1ab
James Chapman <jchapman605@gmail.com>
2024-08-30 11:51:57 -0600

    draw notes list method created in controller

--- app/controllers/NotesController.js
+++ app/controllers/NotesController.js
@@ -7,7 +7,19 @@ import { setHTML } from "../utils/Writer.js";
 export class NotesController {
   constructor() {
     console.log('the notes controller is loaded')
+
+
+
+
   }
+
+  drawNotesList() {
+    const Notes = AppState.notes
+    let notesListHTML = ''
+    Notes.forEach(note => notesListHTML += note.listHTMLTemplate)
+    setHTML('list-area', notesListHTML)
+  }
+
 
 
 }
